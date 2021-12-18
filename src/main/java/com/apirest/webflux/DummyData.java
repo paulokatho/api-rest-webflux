7c60d7b3252da117eb17c6523f6c76f3a53f6e85
paulo-pay <paulo.fernandes@payshopx.com.br>
2021-12-17 23:29:15 -0300

    feat: Creation Router and Handler class and adjustments

--- src/main/java/com/apirest/webflux/DummyData.java
+++ src/main/java/com/apirest/webflux/DummyData.java
@@ -9,25 +9,25 @@ import com.apirest.webflux.document.Playlist;
 import com.apirest.webflux.repository.PlaylistRepository;
 
 import reactor.core.publisher.Flux;
-//
-//@Component
-//public class DummyData implements CommandLineRunner {
-//
-//	private final PlaylistRepository playlistRepository;
-//	
-//    DummyData(PlaylistRepository playlistRepository) {
-//        this.playlistRepository = playlistRepository;
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//    	playlistRepository.deleteAll()
-//                .thenMany(
-//                        Flux.just("API REST Spring Boot", "Deploy de uma aplicação java no IBM Cloud", "Java 8",
-//                                "Github", "Spring Security", "Web Service RESTFULL", "Bean no Spring Framework")
-//                                .map(nome -> new Playlist(UUID.randomUUID().toString(), nome))
-//                                .flatMap(playlistRepository::save))                				
-//                .subscribe(System.out::println);
-//    }
-//}
+
+@Component
+public class DummyData implements CommandLineRunner {
+
+	private final PlaylistRepository playlistRepository;
+	
+    DummyData(PlaylistRepository playlistRepository) {
+        this.playlistRepository = playlistRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    	playlistRepository.deleteAll()
+                .thenMany(
+                        Flux.just("API REST Spring Boot", "Deploy de uma aplicação java no IBM Cloud", "Java 8",
+                                "Github", "Spring Security", "Web Service RESTFULL", "Bean no Spring Framework")
+                                .map(nome -> new Playlist(UUID.randomUUID().toString(), nome))
+                                .flatMap(playlistRepository::save))                				
+                .subscribe(System.out::println);
+    }
+}
